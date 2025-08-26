ea6653f76a759e0e733868fa48d46856bb454cf8
paritoshAuminus <paritoshverma2038@gmail.com>
2025-08-26 16:50:39 +0530

    e-commerce-website initial commit

--- 12-e-commerce-wesite/my-react-app/src/App.jsx
+++ 12-e-commerce-wesite/my-react-app/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/'/>
+        <Route path='/' Component={<Home/>} />
       </Routes>
     </>
   )
--- 12-e-commerce-wesite/my-react-app/src/components/Header.jsx
+++ 12-e-commerce-wesite/my-react-app/src/components/Header.jsx
@@ -1,22 +1,19 @@
+// Eden aura is the title of the site
 import React from 'react'
-import { Link, Links } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
-        <header>
-            <title>Eden Aura</title>
-            <nav>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/shop'}>Shop</Link>
-                <Link to={'/about'}>About</Link>
-                <Link to={'/contact'}>Contact</Link>
-                <Link to={'/f'}>fb</Link>
-                <Link to={'/ig'}>ig</Link>
-                <Link to={'/yt'}>yt</Link>
-                <Link to={'/x'}>x</Link>
-            </nav>
-        </header>
+      <nav className='flex justify-between mx-14 my-3'>
+        <h1>Eden Aura</h1>
+        <ul className='flex justify-around gap-7'>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/shop'>Shop</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+      </nav>
     </>
   )
 }
